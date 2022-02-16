de4c8e28fb2fdb48e2a615b39f49ddce2236bae5
noga dayan <110382@st-maartenscollege,nl>
2022-02-16 13:26:21 +0100

    Add project files.

---
+++ Viva Clothing/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Viva_Clothing.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
