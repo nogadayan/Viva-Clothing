599230b65e6e96183458303d9aae717cd3b608ae
MaudienSmittenaar <57255283+MaudienSmittenaar@users.noreply.github.com>
2022-03-30 15:34:53 +0200

    klant.cs aangemaakt

---
+++ Viva Clothing/Models/Klant.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Viva_Clothing.Models
+{
+    public class Klant
+    {
+        public string Voornaam { get; set; }
+
+        public string Achternaam { get; set; }
+
+        public string Email { get; set; }
+        public string Vraag { get; set; }
+        public string Wachtwoord { get; set; }
+
+    }
+}
