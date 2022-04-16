ad4ad07282f3857ad4b3593efe7ac359da6547a1
noga dayan <110382@st-maartenscollege,nl>
2022-04-16 16:37:06 +0200

    Maat.cs aangemaakt

---
+++ Viva Clothing/Models/Maat.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Viva_Clothing.Models
+{
+    public class Maattabel
+    {
+        public string Id { get; set; }
+
+        public string Product_id { get; set; }
+        public string Maat { get; set; }
+        public string Voorraad {get; set; } 
+    }
+}
